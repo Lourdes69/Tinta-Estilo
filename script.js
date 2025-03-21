b95cc6495a7ca96bc3b0e55ed56424766ca810cc
Lourdes Avalos <lulyavalosmartinez@gmail.com>
2025-03-21 15:10:07 -0300

    actualizando enlaces de productos a Amazon

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
     const products = [
-        { name: "Tatuajes temporales de flores", image: "https://m.media-amazon.com/images/I/81XSl68BPEL._SX425_.jpg", link: "https://a.co/d/ivG0YuX" },
-        { name: "Tatuajes temporales de flores 3D", image: "https://m.media-amazon.com/images/I/71Ogpo19tGL._SX425_.jpg", link: "https://a.co/d/0nTghqZ" },
-        { name: "Tatuajes temporales de flores de acuarela", image: "https://m.media-amazon.com/images/I/71GbYfiz+lL._SX425_.jpg", link: "https://a.co/d/ieiPpvn" },
-        { name: "Tatuajes temporales de acuarela con flores y lavanda", image: "https://m.media-amazon.com/images/I/81zZQHPUn8L._SX425_.jpg", link: "https://a.co/d/j3ttdsM" },
-        { name: "Tatuajes temporales de flores y mariposas", image: "https://m.media-amazon.com/images/I/71PP5-JsWlL._SX425_.jpg", link: "https://a.co/d/jcakaje" },
-        { name: "Tatuajes temporales de mariposa", image: "https://m.media-amazon.com/images/I/61rQyITr8wL._SX425_.jpg", link: "https://a.co/d/20QERK1" },
-        { name: "Tatuajes temporales de Águilas", image: "https://m.media-amazon.com/images/I/71qF3JR3uJL._SX425_.jpg", link: "https://a.co/d/hCHxfhw" },
-        { name: "Tatuajes temporales de flores, mariposas y serpientes", image: "https://m.media-amazon.com/images/I/71d9yTUgDEL._SX425_.jpg", link: "https://a.co/d/dUaO5iX" }
+        { name: "Tatuajes temporales de flores", image: "https://m.media-amazon.com/images/I/81XSl68BPEL._SX425_.jpg", link: "https://amzn.to/41Llp54" },
+        { name: "Tatuajes temporales de flores 3D", image: "https://m.media-amazon.com/images/I/71Ogpo19tGL._SX425_.jpg", link: "https://amzn.to/4bM8695" },
+        { name: "Tatuajes temporales de flores de acuarela", image: "https://m.media-amazon.com/images/I/71GbYfiz+lL._SX425_.jpg", link: "https://amzn.to/4kYINVw" },
+        { name: "Tatuajes temporales de acuarela con flores y lavanda", image: "https://m.media-amazon.com/images/I/81zZQHPUn8L._SX425_.jpg", link: "https://amzn.to/4htTSuD" },
+        { name: "Tatuajes temporales de flores y mariposas", image: "https://m.media-amazon.com/images/I/71PP5-JsWlL._SX425_.jpg", link: "https://amzn.to/4kOYZbS" },
+        { name: "Tatuajes temporales de mariposa", image: "https://m.media-amazon.com/images/I/61rQyITr8wL._SX425_.jpg", link: "https://amzn.to/4kJO6YL" },
+        { name: "Tatuajes temporales de Águilas", image: "https://m.media-amazon.com/images/I/71qF3JR3uJL._SX425_.jpg", link: "https://amzn.to/41HsrHU" },
+        { name: "Tatuajes temporales de flores, mariposas y serpientes", image: "https://m.media-amazon.com/images/I/71d9yTUgDEL._SX425_.jpg", link: "https://amzn.to/4kKRToC" },
+        { name: "Tatuajes temporales de acuarela para mujeres", image: "https://m.media-amazon.com/images/I/71nX3ETqjuL._SX425_.jpg", link: "https://amzn.to/4kM5sUQ" }
     ];
 
     const container = document.getElementById("product-list");
